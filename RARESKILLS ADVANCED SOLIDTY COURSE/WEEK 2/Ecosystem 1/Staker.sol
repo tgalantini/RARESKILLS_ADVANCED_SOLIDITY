f91964c30c702857376b15f39db6de1cbecfb964
tgalantini <92930673+tgalantini@users.noreply.github.com>
2024-10-07 13:25:12 +0200

    Add files via upload
    Updating staker.sol logic

--- RARESKILLS ADVANCED SOLIDTY COURSE/WEEK 2/Ecosystem 1/Staker.sol
+++ RARESKILLS ADVANCED SOLIDTY COURSE/WEEK 2/Ecosystem 1/Staker.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.27;
 
 /// @author Tommaso Galantini
-/// @title A ERC721 staking contract with ERC20 rewards
+/// @title A Erc20 Token for staking rewards
 
 import "@openzeppelin/contracts@5.0.0/access/Ownable2Step.sol";
 import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
@@ -15,7 +15,9 @@ interface IERC20Mint {
 
 contract SimpleStaking is IERC721Receiver, Ownable2Step{
 
-    event deposited
+    event Deposit(address indexed user, uint256 indexed tokenId);
+    event Withdraw(address indexed user, uint256 indexed tokenId);
+    event RewardsClaimed(address indexed user, uint256 amount, uint256 timestamp);
 
     struct Stake {
         address Owner;
@@ -27,6 +29,7 @@ contract SimpleStaking is IERC721Receiver, Ownable2Step{
 
     uint256 public constant REWARD_RATE = 10 * 10**18; // 10 tokens per 24 hours (with 18 decimals)
     uint256 public constant ONE_DAY = 1 days;
+    uint256 public constant REWARD_RATE_PER_SECOND = REWARD_RATE / ONE_DAY;
 
     mapping (uint256 tokenId => Stake) public stakes;
 
@@ -38,35 +41,32 @@ contract SimpleStaking is IERC721Receiver, Ownable2Step{
     function withdrawNft(uint256 tokenId) public {
         address originalOwner = stakes[tokenId].Owner;
         require(originalOwner == msg.sender, "Not owner");
-        uint256 reward = (block.timestamp - stakes[tokenId].timestamp) % ONE_DAY * REWARD_RATE; // rewards accrued
+        claimRewards(tokenId);
         delete stakes[tokenId]; 
-        rewardsToken.mint(msg.sender, reward);
         IERC721(address(stakingNft)).transferFrom(address(this), msg.sender, tokenId); // withdraw NFT
+        emit Withdraw(originalOwner, tokenId);
     }
 
-    function calculateRewards1() public pure returns(uint256){
-        uint256 reward = 5 hours / ONE_DAY * REWARD_RATE; 
-        return reward;
-        }
-    
-    function calculateRewards2() public pure returns(uint256){
-        uint256 reward = 5 hours % ONE_DAY * REWARD_RATE; 
-        return reward;
-    }
 
     function withdrawRewards(uint256 tokenId) public {
         address originalOwner = stakes[tokenId].Owner;
         require(originalOwner == msg.sender, "Not owner");
-        require(block.timestamp - stakes[tokenId].timestamp >= ONE_DAY, "Not enough time passed since last claim");
-        uint256 reward = (block.timestamp - stakes[tokenId].timestamp) % ONE_DAY * REWARD_RATE; // rewards per day
+        claimRewards(tokenId);
+    }
+
+    function claimRewards(uint256 tokenId) internal {
+        address originalOwner = stakes[tokenId].Owner;
+        uint256 reward = (block.timestamp - stakes[tokenId].timestamp) * REWARD_RATE_PER_SECOND; // rewards per day
         require(reward > 0, "No rewards accrued");
         stakes[tokenId].timestamp = block.timestamp; // timestamp update
         rewardsToken.mint(msg.sender, reward);
+        emit RewardsClaimed(originalOwner, reward, block.timestamp);
     }
 
     function depositAndStake(address depositor, uint256 tokenId) private {
         stakes[tokenId].Owner = depositor;
         stakes[tokenId].timestamp = block.timestamp;
+        emit Deposit(depositor, tokenId);
     }
 
     function onERC721Received(address,
